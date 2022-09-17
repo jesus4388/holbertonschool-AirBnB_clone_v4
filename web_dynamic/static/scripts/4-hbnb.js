4b462910293d4fa7e2ad7c94e30665e7733ab6da
Alex <asenges400@gmail.com>
2022-09-17 08:23:45 -0300

    Task 3 and 4

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,67 @@
+$(document).ready(function () {
+  const dict = {};
+  $('input:checkbox').change(function () {
+    if (this.checked) {
+      dict[$(this).attr('data-id')] = $(this).attr('data-name');
+      $('.amenities h4').empty();
+    } else {
+      delete dict[$(this).attr('data-id')];
+      $('.amenities h4').empty();
+    }
+    let count = Object.keys(dict).length;
+    for (const [key, value] of Object.entries(dict)) {
+      if (count > 1) {
+        $('.amenities h4').append(value, ", ");
+        console.log(dict);
+      } else {
+        $('.amenities h4').append(value);
+      }
+      count--;
+    }
+  });
+  const Url='http://0.0.0.0:5001/api/v1/status';
+  
+  function getstatus() {
+    $.get(Url, function(data, textStatus) {
+      if (data) {
+        $('#api_status').addClass("available");
+      } else {
+        $('#api_status').addClass("available");
+      }
+   });
+  };
+  getstatus()
+});
+
+$.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search',
+    data: '{}',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      for (let i = 0; i < data.length; i++) {
+        let place = data[i];
+        $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+      }
+    }
+  });
+});
+
+$('.filters > button').click(function () {
+    $('.places > article').remove();
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search',
+      data: JSON.stringify({'amenities': Object.keys(checkedAmenities)}),
+      dataType: 'json',
+      contentType: 'application/json',
+      success: function (data) {
+        for (let i = 0; i < data.length; i++) {
+          let place = data[i];
+          $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+        }
+      }
+    });
+  });
+});
